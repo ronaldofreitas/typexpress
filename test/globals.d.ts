5cc7502742e5bf7c72282f82df84d4f9c852c5cd
Ronaldo Freitas <ronafreitasweb@gmail.com>
2020-08-19 14:50:40 -0300

    add server file and style fix

--- test/globals.d.ts
+++ test/globals.d.ts
@@ -1,6 +1,6 @@
 //https://stackoverflow.com/questions/39040108/import-class-in-definition-file-d-ts/51114250#51114250
 declare namespace NodeJS {
-    interface Global {
-        testRequest: import('supertest').Supertest<import('supertest').Test>;
-    }
+  interface Global {
+    testRequest: import('supertest').Supertest<import('supertest').Test>;
+  }
 }
